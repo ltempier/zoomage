f1447703716dfc7c094d3f790f2867bf8731a3a6
laurent <ltr@momagroup.com>
2017-04-24 15:22:12 +0200

    add console.log PUB

--- client/index.js
+++ client/index.js
@@ -1,14 +1,16 @@
 "use strict";
 
+console.log('Code here: https://github.com/ltempier/zoomage \n\nThanks for your visit!');
+
 $(document).ready(function () {
 
     var workspace = new Zoomage('zoomage', function (gifDataUrl) {
         $('#gif-result').attr('src', gifDataUrl);
 
-        if (gifDataUrl && gifDataUrl.length){
+        if (gifDataUrl && gifDataUrl.length) {
             $('#download').show()
             $('#download').attr('href', gifDataUrl)
-        }else
+        } else
             $('#download').hide()
     });
 
